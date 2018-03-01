dbee423a0d360c39c547d23d16e09be519cb3556
op880623 <op880623@gmail.com>
2018-03-01 23:41:32 +0800

    add Comic functions(new, update, episodes)

--- comic/models.py
+++ comic/models.py
@@ -1,14 +1,90 @@
+import re
+import requests
+
 from django.db import models
-from django.db.models.deletion import PROTECT, CASCADE
+from django.db.models.deletion import SET_NULL, CASCADE
 
 class Comic(models.Model):
-    comicId = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
-    url = models.CharField(max_length=200)
-    newest = models.ForeignKey('Episode', on_delete=PROTECT, related_name='newest')
-    progress = models.ForeignKey('Episode', on_delete=PROTECT, related_name='progress')
+    comicId = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length = 100)
+    url = models.CharField(max_length = 200)
+    newest = models.ForeignKey(
+        'Episode',
+        blank = True,
+        null = True,
+        on_delete = SET_NULL,
+        related_name = 'newest'
+    )
+    progress = models.ForeignKey(
+        'Episode',
+        blank = True,
+        null = True,
+        on_delete = SET_NULL,
+        related_name = 'progress'
+    )
+
+    @classmethod
+    def new(cls, comicId):
+        # create or get model
+        if Comic.objects.filter(comicId = comicId).exists():
+            comic = Comic.objects.get(comicId = comicId)
+        else:
+            comic = cls(comicId = comicId)
+
+        # find url
+        domain = "http://www.cartoonmad.com"
+        comicId = str(comic.comicId)
+        comic.url = "%s/comic/%s.html" % (domain, comicId)
+
+        # find name
+        page = requests.get(comic.url)
+        page.encoding = 'big5'
+        pattern = '<a href=/comic/%s.html>(.+)</a>' % comicId
+        comic.name = re.search(pattern, page.text).group(1)
+
+        # save and return
+        comic.save()
+        return comic
+
+    def update(self, progress = None):
+        # determine progress number
+        try:
+            if progress is None:
+                progress = self.progress.index
+        except:
+            progress = 0
+
+        # get page
+        page = requests.get(self.url)
+        page.encoding = 'big5'
+
+        # build episodes
+        pattern = 'href=(/comic/%s(\d{4})\d{7}.html)' % str(self.comicId)
+        for a_tag in re.findall(pattern, page.text):
+            index = int(a_tag[1])
+            if index >= progress:
+                if not Episode.objects.filter(index = index, comic = self).exists():
+                    e = Episode(index = index, url = a_tag[0], comic = self)
+                    e.save()
+
+        # find progress episode
+        self.progress = Episode.objects.filter(index = progress, comic = self).get()
+
+        # find newest episode
+        newest = 0
+        for ep in Episode.objects.filter(comic = self):
+            if ep.index > newest:
+                newest = ep.index
+        self.newest = Episode.objects.filter(index = newest, comic = self).get()
+
+        # save and return
+        self.save()
+        return None
+
+    def episodes(self):
+        return Episode.objects.filter(comic = self)
 
 class Episode(models.Model):
     index = models.IntegerField()
-    url = models.CharField(max_length=200)
-    comic = models.ForeignKey(Comic, on_delete=CASCADE)
+    url = models.CharField(max_length = 200)
+    comic = models.ForeignKey(Comic, on_delete = CASCADE)
