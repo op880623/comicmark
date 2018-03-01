92b15d4c1f5e55a3e24652f5bfebef9cd10f6dfa
op880623 <op880623@gmail.com>
2018-03-02 00:46:02 +0800

    update template

--- comic/views.py
+++ comic/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Comic
+
 def index(request):
-    return render(request, 'comic/index.html')
+    comics = Comic.objects.all()
+    return render(request, 'comic/index.html', {'comics': comics})
