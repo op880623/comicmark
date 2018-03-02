0c66d64fcba24f1e225475ab9bc3abf64cb22c6f
op880623 <op880623@gmail.com>
2018-03-02 13:40:54 +0800

    add updateall command

---
+++ comic/management/commands/updateall.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand, CommandError
+from comic.models import Comic
+
+class Command(BaseCommand):
+    help = "Update all comics"
+
+    def handle(self, *args, **options):
+        for comic in Comic.objects.all():
+            comic.update()
