5103b9d2618a546a2592c7d562a94fff0108b1f9
op880623 <op880623@gmail.com>
2018-03-26 14:46:01 +0800

    automatically change to mobile url

--- comic/static/comic.js
+++ comic/static/comic.js
@@ -1,4 +1,16 @@
+function isMobileDevice() {
+  return (typeof window.orientation !== "undefined")
+      || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
+
 $(document).ready(function() {
+  if (isMobileDevice()) {
+    for (var i = 0; i < $("a[target=_blank]").length; i++){
+      a = $("a[target=_blank]")[i];
+      a.pathname = "/m" + a.pathname;
+    }
+  }
+
   $("button.read-now").click(function(event) {
     event.target.parentElement.parentElement.children[0].click();
     event.target.parentElement.children[0].click();
